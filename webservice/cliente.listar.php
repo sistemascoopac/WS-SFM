720af787c42cc266c0e19f674e58293eb5f30b37
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-01-10 17:08:22 -0500

    Update cliente.listar.php

--- webservice/cliente.listar.php
+++ webservice/cliente.listar.php
@@ -17,8 +17,8 @@ try {
         $obj = new Cliente();
         
         $resultado = $obj->Listar($p_ccod_cliente);
-        //$foto = $obj->obtenerFoto($p_ccod_cliente);
-        //$resultado["foto"] = $foto;
+        $foto = $obj->obtenerFoto($p_ccod_cliente);
+        $resultado["foto"] = $foto;
     //$resultado["gg"]="ohno";
         Funciones::imprimeJSON(200, "", $resultado);
     }
