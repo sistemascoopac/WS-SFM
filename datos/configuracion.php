e7786038a24d6619277b27352a678abd999270a1
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-04 17:26:08 -0500

    Update configuracion.php

--- datos/configuracion.php
+++ datos/configuracion.php
@@ -1,9 +1,8 @@
 <?php
 
-define("SERVIDOR_BD", "ec2-54-225-150-216.compute-1.amazonaws.com");
-define("USUARIO_BD", "awnjrnwnyqgxlq");
-define("CLAVE_BD", "e4d2c38f3376cba53edddc73667bd7a27ff1805446b65194a46e100e6592d273");
+define("SERVIDOR_BD", "190.233.55.167");
+define("USUARIO_BD", "sa");
+define("CLAVE_BD", "CSFM_2018*");
 define("PUERTO_BD", "5432");
-define("NOMBRE_BD", "d2r684jf2fmsaa");
- 
+define("NOMBRE_BD", "bd_sfm_astudio");
 
