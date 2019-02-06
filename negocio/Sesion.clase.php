b08132cc5927c814525720e489acf593aab4abf1
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-06 17:29:49 -0500

    Update Sesion.clase.php

--- negocio/Sesion.clase.php
+++ negocio/Sesion.clase.php
@@ -27,58 +27,25 @@ class Sesion extends Conexion {
      public function validarSesion() {
         try {
             
-            $serverName = "190.233.55.167";
-    $connectionInfo = array( "Database"=>"bd_sfm_astudio", "UID"=>"sa", "PWD"=>"CSFM_2018*");
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
-    if( $conn === false )
-    {
-        echo "Could not connect.\n";
-        print('<pre>');
-        die( print_r( sqlsrv_errors(), true));
-        print('</pre>');
-    }
+          
             
-            $tsql_callSP = "{call prc_inicio_sesion(?,?)}";
+           $sql=" exec bd_sfm_astudio.dbo.prc_inicio_sesion @p_usuario='rperez' , @p_clave='202cb962ac59075b964b07152d234b70' ";
             
-            
-            
-             $usuario= $this->getUsuario();
-            $clave= $this->getClave();
-            
-            $params = array(
-                array($usuario, SQLSRV_PARAM_IN),
-                array($clave, SQLSRV_PARAM_IN)
-                            );
+            $sentencia = $this->dblink->prepare($sql);
            
-      $stmt3 = sqlsrv_query( $conn, $tsql_callSP, $params);
-    if( $stmt3 === false )
-    {
-        echo "Error in executing statement 3.\n";
-        die( print_r( sqlsrv_errors(), true));
-    }
-        return $stmt3;
-    /*Free the statement and connection resources. */
-    //sqlsrv_free_stmt($stmt3);
-    sqlsrv_close($conn);
-            
-            
-           //$sql=" call bd_sfm_astudio.dbo.prc_inicio_sesion @p_usuario='rperez' , @p_clave='202cb962ac59075b964b07152d234b70' ";
-            
-           // $sentencia = $this->dblink->prepare($sql);
            
-           /* 
             
             $usuario= $this->getUsuario();
             $clave= $this->getClave();
-            mssql_bind($sp, "@p_usuario", $usuario, SQLVARCHAR);
-            mssql_bind($sp, "@p_clave", $clave, SQLVARCHAR);
-            //$sentencia->bindParam(":p_usuario", $usuario);
-           // $sentencia->bindParam(":p_clave", $clave);
-            //$sentencia->execute();
-            //return $sentencia->fetch(PDO::FETCH_ASSOC);
-           return $data = mssql_fetch_row(mssql_execute($sp));
+           // mssql_bind($sp, "@p_usuario", $usuario, SQLVARCHAR);
+           // mssql_bind($sp, "@p_clave", $clave, SQLVARCHAR);
+            $sentencia->bindParam(":p_usuario", $usuario);
+            $sentencia->bindParam(":p_clave", $clave);
+            $sentencia->execute();
+            return $sentencia->fetch(PDO::FETCH_ASSOC);
+          // return $data = mssql_fetch_row(mssql_execute($sp));
             
-            */
+            
             
         } catch (Exception $exc) {
             throw $exc->getTraceAsString();
