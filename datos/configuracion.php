513d9f3b76613c8d9a6036298254ab7d98e5a7b6
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-09 09:28:19 -0500

    Update configuracion.php

--- datos/configuracion.php
+++ datos/configuracion.php
@@ -1,8 +1,8 @@
 <?php
 
-define("SERVIDOR_BD", "190.233.55.167");
+define("SERVIDOR_BD", "coopacmocupe.ddns.net");
 define("USUARIO_BD", "sa");
 define("CLAVE_BD", "CSFM_2018*");
 define("PUERTO_BD", "5432");
-define("NOMBRE_BD", "bd_sfm_astudio");
+define("NOMBRE_BD", "bd_sfm_astudio2");
 
