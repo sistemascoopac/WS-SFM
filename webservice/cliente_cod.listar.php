c83eec87851f6488ff6d3f06b2a46d703ce1c2a5
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-09 11:35:52 -0500

    Update cliente_cod.listar.php

--- webservice/cliente_cod.listar.php
+++ webservice/cliente_cod.listar.php
@@ -22,8 +22,7 @@ try {
         
         $datosSocios = array(
             "ccod_cliente" => $resultado[$i]["ccod_cliente"],
-            "nombre_completo" => $resultado[$i]["nombre_completo"],
-            "ccod_cliente" => $resultado[$i]["Ccod_cliente"],
+            "nombre_completo" => $resultado[$i]["nombre_completo"],            
             "cdni_cliente" => $resultado[$i]["Cdni_cliente"],
             "cdir_cliente" => $resultado[$i]["Cdir_cliente"],
             "ctel_cliente" => $resultado[$i]["Ctel_cliente"],
