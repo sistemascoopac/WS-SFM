41bfd2a87c119b4bf07bdec90e1e8cf7d4fb5350
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-09 11:37:04 -0500

    Update cliente_cod.listar.php

--- webservice/cliente_cod.listar.php
+++ webservice/cliente_cod.listar.php
@@ -21,7 +21,7 @@ try {
             $foto = $obj->obtenerFoto($resultado[$i]["Ccod_cliente"]);
         
         $datosSocios = array(
-            "ccod_cliente" => $resultado[$i]["ccod_cliente"],
+            "ccod_cliente" => $resultado[$i]["Ccod_cliente"],
             "nombre_completo" => $resultado[$i]["nombre_completo"],            
             "cdni_cliente" => $resultado[$i]["Cdni_cliente"],
             "cdir_cliente" => $resultado[$i]["Cdir_cliente"],
