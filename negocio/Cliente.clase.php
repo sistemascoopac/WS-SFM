d669febfe9445e6f594f54b2e662aadba31418e5
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-11 18:47:39 -0500

    Update Cliente.clase.php

--- negocio/Cliente.clase.php
+++ negocio/Cliente.clase.php
@@ -409,7 +409,7 @@ class Cliente extends Conexion {
 	
 	
 	public function  obtenerFoto($ccod){
-        $foto = "/imagenes_socios/".$ccod;
+        $foto = "/imagenes_socios/".$ccod.".JPG";
         if(file_exists($foto.".JPG")){
             $foto = $foto.".JPG";
         }else{
