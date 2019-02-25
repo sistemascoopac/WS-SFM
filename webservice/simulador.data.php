f2f2ffa58e304c47513e17260999d1b41fca4391
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-25 11:52:06 -0500

    Add files via upload

---
+++ webservice/simulador.data.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../negocio/Simulador.clase.php';
+require_once '../util/funciones/Funciones.clase.php';
+require_once 'token.validar.php';
+
+if (! isset($_POST["token"])){
+    Funciones::imprimeJSON(500, "Debe especificar un token", "");
+    exit();
+}
+
+$token = $_POST["token"];
+$p_producto = $_POST["p_producto"];
+$p_monto = $_POST["p_monto"];
+$p_ncuotas = $_POST["p_ncuotas"];
+try {
+    
+        $obj = new Simulador();
+        $resultado = $obj->GetData($p_producto, $p_monto, $p_ncuotas);
+        Funciones::imprimeJSON(200, "", $resultado);
+    
+    
+} catch (Exception $exc) {
+    
+    Funciones::imprimeJSON(500, $exc->getMessage(), "");
+}
