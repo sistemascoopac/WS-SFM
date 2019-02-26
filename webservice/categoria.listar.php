65427a85bff932b5cd46620dadeba29f571b3cf6
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-02-26 17:16:01 -0500

    Add files via upload

---
+++ webservice/categoria.listar.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once '../negocio/Categoria.clase.php';
+require_once '../util/funciones/Funciones.clase.php';
+require_once 'token.validar.php';
+
+if (! isset($_POST["token"])){
+    Funciones::imprimeJSON(500, "Debe especificar un token", "");
+    exit();
+}
+
+$token = $_POST["token"];
+try {
+    //if(validarToken($token)){
+        $obj = new Categoria();
+        $resultado = $obj->listar();
+        
+        Funciones::imprimeJSON(200, "", $resultado);
+   // }
+    
+    
+} catch (Exception $exc) {
+    
+    Funciones::imprimeJSON(500, $exc->getMessage(), "");
+}
