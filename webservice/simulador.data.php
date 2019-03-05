59291cfa3d62f694e7ad7dc0f84270d80a9e69fd
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-03-05 17:05:09 -0500

    Update simulador.data.php

--- webservice/simulador.data.php
+++ webservice/simulador.data.php
@@ -2,12 +2,9 @@
 
 require_once '../negocio/Simulador.clase.php';
 require_once '../util/funciones/Funciones.clase.php';
-require_once 'token.validar.php';
 
-if (! isset($_POST["token"])){
-    Funciones::imprimeJSON(500, "Debe especificar un token", "");
-    exit();
-}
+
+
 
 $token = $_POST["token"];
 $p_producto = $_POST["p_producto"];
@@ -17,7 +14,7 @@ $p_indicador=$_POST["p_indicador];
 try {
     
         $obj = new Simulador();
-        $resultado = $obj->GetData($p_producto, $p_monto, $p_ncuotas,$p_indicador);
+        $resultado = $obj->GetData($p_producto, $p_monto, $p_ncuotas, $p_indicador);
         Funciones::imprimeJSON(200, "", $resultado);
     
     
