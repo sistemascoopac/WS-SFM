90180183753ec62b3d936793457101ffdb171655
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-03-05 13:08:15 -0500

    Update simulador.data.php

--- webservice/simulador.data.php
+++ webservice/simulador.data.php
@@ -13,10 +13,11 @@ $token = $_POST["token"];
 $p_producto = $_POST["p_producto"];
 $p_monto = $_POST["p_monto"];
 $p_ncuotas = $_POST["p_ncuotas"];
+$p_indicador=$_POST["p_indicador];
 try {
     
         $obj = new Simulador();
-        $resultado = $obj->GetData($p_producto, $p_monto, $p_ncuotas);
+        $resultado = $obj->GetData($p_producto, $p_monto, $p_ncuotas,$p_indicador);
         Funciones::imprimeJSON(200, "", $resultado);
     
     
