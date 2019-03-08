a10a55ad5780244c33b1e6a1207bd3e2111c3269
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-03-07 19:38:36 -0500

    Update cambiar.pw.php

--- webservice/cambiar.pw.php
+++ webservice/cambiar.pw.php
@@ -3,7 +3,18 @@
 require_once '../negocio/Sesion.clase.php';
 require_once '../util/funciones/Funciones.clase.php';
 
-
+if (! isset($_POST["usuario"])){
+    Funciones::imprimeJSON(500, "No se especificó usuario", "");
+    exit();
+}
+if (! isset($_POST["clave"])){
+    Funciones::imprimeJSON(500, "Ingrese clave", "");
+    exit();
+}
+if (! isset($_POST["nclave"])){
+    Funciones::imprimeJSON(500, "Ingrese la clave nueva", "");
+    exit();
+}
 $usuario = $_POST["usuario"];
 $clave = $_POST["clave"];
 $nclave = $_POST["nclave"];
