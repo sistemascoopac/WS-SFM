2e40f7c71797edfec12090bfbb4df3e5e1401e0a
sistemascoopac <46034752+sistemascoopac@users.noreply.github.com>
2019-03-07 19:29:18 -0500

    Update Sesion.clase.php

--- negocio/Sesion.clase.php
+++ negocio/Sesion.clase.php
@@ -6,6 +6,15 @@ class Sesion extends Conexion {
 
     private $usuario;
     private $clave;
+    private $n_clave;
+    
+    function getN_clave() {
+        return $this->n_clave;
+    }
+
+    function setN_clave($n_clave) {
+        $this->n_clave = $n_clave;
+    }
 
     function getUsuario() {
         return $this->usuario;
@@ -22,6 +31,7 @@ class Sesion extends Conexion {
     function setClave($clave) {
         $this->clave = $clave;
     }
+    
 
     
      public function validarSesion() {
@@ -51,6 +61,33 @@ class Sesion extends Conexion {
             throw $exc->getTraceAsString();
         }
     }
+    
+    public function cambiarpw(){
+         try {
+        $sql=" SET NOCOUNT ON exec bd_sfm_astudio2.dbo.prc_cambiarpw @p_usuario=:p_usuario , @p_clave=:p_clave , @p_nclave=:p_nclave ";
+            
+            $sentencia = $this->dblink->prepare($sql);
+           
+           
+            
+            $usuario= $this->getUsuario();
+            $clave= $this->getClave();
+            $nclave=$this->getN_clave();
+           // mssql_bind($sp, "@p_usuario", $usuario, SQLVARCHAR);
+           // mssql_bind($sp, "@p_clave", $clave, SQLVARCHAR);
+            $sentencia->bindParam(":p_usuario", $usuario);
+            $sentencia->bindParam(":p_clave", $clave);
+            $sentencia->bindParam(":p_nclave",$nclave);
+            $sentencia->execute();
+            return $sentencia->fetch(PDO::FETCH_ASSOC);
+          // return $data = mssql_fetch_row(mssql_execute($sp));
+            
+            
+            
+        } catch (Exception $exc) {
+            throw $exc->getTraceAsString();
+        } 
+}
     
     public function  obtenerFoto($dni){
         $foto = "../imagenes-usuario/".$dni;
